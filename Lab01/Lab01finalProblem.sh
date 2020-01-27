263f9ed22cef25fba63bd1cb3ec6d296a154c6cd
Zach-git <zagi6162@colorado.edu>
2020-01-27 11:01:19 -0700

    Edited script/ removes row # on the tail

--- Lab01/Lab01finalProblem.sh
+++ Lab01/Lab01finalProblem.sh
@@ -1,3 +1,3 @@
 cut -f 2-4 -d , PredPreyData.csv | head -1 > New_PredPreyData.csv 
-tail -10 PredPreyData.csv >> New_PredPreyData.csv
+cut -f 2-4 -d , PredPreyData.csv | tail -10 PredPreyData.csv >> New_PredPreyData.csv
 
